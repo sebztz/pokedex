5911382ce045f90f26dcec07228b932838d4e9e8
sebastien_lacour <sebastien-lacour@hotmail.com>
2023-10-10 11:03:21 +0200

    react_22

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,5 +1,5 @@
 import PokemonCard from './components/PokemonCard';
-import './App.css';
+
 
 function App() {
 
--- pokemon/src/components/PokemonCard.jsx
+++ pokemon/src/components/PokemonCard.jsx
@@ -2,9 +2,9 @@ function PokemonCard(){
 
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+            <figcaption> Bulbasaur </figcaption>
         </figure>
-        <figcaption>"Bulbasaur"</figcaption>
     );
 }
     export default PokemonCard 
