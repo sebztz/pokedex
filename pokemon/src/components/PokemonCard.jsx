6a1628ef247f90a00c4d3f2e8ebc67269fbb61a2
sebastien_lacour <sebastien-lacour@hotmail.com>
2023-10-11 10:38:38 +0200

    react03end

--- pokemon/src/components/PokemonCard.jsx
+++ pokemon/src/components/PokemonCard.jsx
@@ -1,10 +1,25 @@
-function PokemonCard(){
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgScr:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+]
 
-    return (
+function PokemonCard(){
+    let pokemon = pokemonList[0]
+    
+    return (  
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-            <figcaption> Bulbasaur </figcaption>
-        </figure>
-    );
+           {pokemon.imgScr ? <img src={pokemon.imgScr} alt={pokemon.name}/>   : <h1>???</h1>}
+            <figcaption> {pokemon.name} </figcaption>
+        </figure>        
+    )
+     
 }
+ 
+
     export default PokemonCard 
